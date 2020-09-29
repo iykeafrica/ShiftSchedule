dd25bd09adde3ec849eb9f728b1e254cdaeb8be0
iykeafrica <54163350+iykeafrica@users.noreply.github.com>
2020-09-29 04:51:30 +0100

    All shifts added remaining to add preferences

--- app/src/main/java/com/example/calendertestapp/WelcomeActivity.java
+++ app/src/main/java/com/example/calendertestapp/WelcomeActivity.java
@@ -3,7 +3,9 @@ package com.example.calendertestapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 
@@ -20,10 +22,15 @@ import com.example.calendertestapp.security.SecurityShiftCActivity;
 public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button plantShiftA, plantShiftB, plantShiftC, cMtceShiftA, cMtceShiftB, cMtceShiftC, securityShiftA, securityShiftB, securityShiftC;
+    private SharedPreferences.Editor mEditor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = pref.edit();
 
         plantShiftA = findViewById(R.id.button);
         plantShiftB = findViewById(R.id.button2);
@@ -35,47 +42,87 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
         securityShiftB = findViewById(R.id.button8);
         securityShiftC = findViewById(R.id.button9);
 
-        plantShiftA.setOnClickListener(this);
-        plantShiftB.setOnClickListener(this);
-        plantShiftC.setOnClickListener(this);
-        cMtceShiftA.setOnClickListener(this);
-        cMtceShiftB.setOnClickListener(this);
-        cMtceShiftC.setOnClickListener(this);
-        securityShiftA.setOnClickListener(this);
-        securityShiftB.setOnClickListener(this);
-        securityShiftC.setOnClickListener(this);
+        if (!pref.getBoolean("first_time_on_boarding", false)) {
+            plantShiftA.setOnClickListener(this);
+            plantShiftB.setOnClickListener(this);
+            plantShiftC.setOnClickListener(this);
+            cMtceShiftA.setOnClickListener(this);
+            cMtceShiftB.setOnClickListener(this);
+            cMtceShiftC.setOnClickListener(this);
+            securityShiftA.setOnClickListener(this);
+            securityShiftB.setOnClickListener(this);
+            securityShiftC.setOnClickListener(this);
+        } else {
+            if (pref.getBoolean("first_time_on_boarding", true)){
+                startActivity(new Intent(this, PlantShiftAActivity.class));
+            } else if (pref.getBoolean("first_time_on_boarding", true)){
+                startActivity(new Intent(this, PlantShiftBActivity.class));
+            } else if (pref.getBoolean("first_time_on_boarding", true)){
+                startActivity(new Intent(this, PlantShiftCActivity.class));
+            } else if (pref.getBoolean("first_time_on_boarding", true)){
+                startActivity(new Intent(this, CMtceShiftAActivity.class));
+            } else if (pref.getBoolean("first_time_on_boarding", true)){
+                startActivity(new Intent(this, CMtceShiftBActivity.class));
+            } else if (pref.getBoolean("first_time_on_boarding", true)){
+                startActivity(new Intent(this, CMtceShiftCActivity.class));
+            } else if (pref.getBoolean("first_time_on_boarding", true)){
+                startActivity(new Intent(this, SecurityShiftAActivity.class));
+            } else if (pref.getBoolean("first_time_on_boarding", true)){
+                startActivity(new Intent(this, SecurityShiftBActivity.class));
+            } else if (pref.getBoolean("first_time_on_boarding", true)){
+                startActivity(new Intent(this, SecurityShiftCActivity.class));
+            }
+        }
     }
 
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.button:
                 startActivity(new Intent(this, PlantShiftAActivity.class));
+                mEditor.putBoolean("first_time_on_boarding", true);
+                mEditor.apply();
                 break;
             case R.id.button2:
                 startActivity(new Intent(this, PlantShiftBActivity.class));
+                mEditor.putBoolean("first_time_on_boarding", true);
+                mEditor.apply();
                 break;
             case R.id.button3:
                 startActivity(new Intent(this, PlantShiftCActivity.class));
+                mEditor.putBoolean("first_time_on_boarding", true);
+                mEditor.apply();
                 break;
             case R.id.button4:
                 startActivity(new Intent(this, CMtceShiftAActivity.class));
+                mEditor.putBoolean("first_time_on_boarding", true);
+                mEditor.apply();
                 break;
             case R.id.button5:
                 startActivity(new Intent(this, CMtceShiftBActivity.class));
+                mEditor.putBoolean("first_time_on_boarding", true);
+                mEditor.apply();
                 break;
             case R.id.button6:
                 startActivity(new Intent(this, CMtceShiftCActivity.class));
+                mEditor.putBoolean("first_time_on_boarding", true);
+                mEditor.apply();
                 break;
             case R.id.button7:
                 startActivity(new Intent(this, SecurityShiftAActivity.class));
+                mEditor.putBoolean("first_time_on_boarding", true);
+                mEditor.apply();
                 break;
             case R.id.button8:
                 startActivity(new Intent(this, SecurityShiftBActivity.class));
+                mEditor.putBoolean("first_time_on_boarding", true);
+                mEditor.apply();
                 break;
             case R.id.button9:
                 startActivity(new Intent(this, SecurityShiftCActivity.class));
+                mEditor.putBoolean("first_time_on_boarding", true);
+                mEditor.apply();
                 break;
             default:
         }
